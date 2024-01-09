7dd9d13bfb54e85b55960b6b0d8b5e54f671e726
pavan <pavanhanjary0810@gmail.com>
2024-01-09 18:03:22 +0400

    Reading the csv file line by line and separating every element of each line

--- library.cpp
+++ library.cpp
@@ -3,22 +3,65 @@
 #include <string>
 #include <vector>
 
+// function to clear the screen
+void clear(){
+  std::cout << "\033c";
+}
+
+// main function
 int main(int argc, char *argv[]){
+  // check if filename argument has been passed
   if (argc != 2) {
     std::cout << "Usage: " << argv[0] << " input-filename" << std::endl;
     exit(1);
   }
-  
+
   std::ifstream file;
   file.open(argv[1]);
+  // check if there an error in opening the file
   if(file.fail()){
     std::cout << "Error to read file\n" << std::endl;
     exit(1);
   }
 
   std::string line;
+  std::string::size_type i;
+  std::string temp="";
+  int count;
+  int bookID;
+  std::string authorFirstName;
+  std::string authorLastName;
+  std::string bookName;
+  int fileLine = 0;
+  // read the file line by line until end of file
   while(getline(file, line)){
-    std::cout << line << std::endl;
+    if (fileLine != 0){
+      count = 1;
+      for(i=0;i < line.length();i++){
+	if(line[i] == ',' || i == (line.length() - 1)){
+	  if (i == line.length() - 1) {
+	    temp = temp + line[i];
+	  }
+	  if (count == 1){
+	    bookID = std::stoi(temp);
+	  } else if(count == 2){
+	    bookName = temp;
+	  } else if(count == 4){
+	    authorFirstName = temp;
+	  } else if(count == 5){
+	    authorLastName = temp;
+	  }
+	  count = count + 1;
+	  temp = "";
+	} else {
+	  temp = temp + line[i];
+	}
+      }
+      std::cout << bookID << std::endl << bookName << std::endl <<
+	  authorFirstName << std::endl << authorLastName << std::endl;
+      std::cout << line[i] << std::endl;
+    }
+    fileLine = fileLine + 1;
   }
   file.close();
   
@@ -34,16 +77,21 @@ int main(int argc, char *argv[]){
     std::cout << "Enter the number for the option: " << std::endl;
     std::cin >> option;
     if (option == 1){
+      clear();
       std::cout << "option1" << std::endl;
     } else if (option == 2){
+      clear();
       std::cout << "option2" << std::endl;
     } else if (option == 3) {
+      clear();
       std::cout << "option3" << std::endl;
     } else if (option == 4) {
+      clear();
       std::cout << "option4" << std::endl;
     } else if (option == 5) {
       exit = 1;
     } else {
+      clear();
       std::cout << "Invalid option. Please try again!\n" << std::endl;
     }
   }
