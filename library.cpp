26b5b34aa59caf1b44f0133d9fa1fb37d55ed1b9
pavan <pavanhanjary0810@gmail.com>
2024-01-09 11:56:57 +0400

    adding program arguments, program usage and reading test file for the books

--- library.cpp
+++ library.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
-int main(){
+int main(int argc, char *argv[]){
+  if (argc != 2) {
+    std::cout << "Usage: " << argv[0] << " input-filename" << std::endl;
+    exit(1);
+  }
+  
+  std::ifstream file;
+  file.open(argv[1]);
+  if(file.fail()){
+    std::cout << "Error to read file\n" << std::endl;
+    exit(1);
+  }
+
+  std::string line;
+  while(getline(file, line)){
+    std::cout << line << std::endl;
+  }
+  file.close();
+  
   int exit = 0;
   int option;
   while (exit != 1){
-    std::cout << "Welcome to the main menu." << std::endl;
+    std::cout << "Welcome to the main menu.\n" << std::endl;
     std::cout << "1. Add Member" << std::endl;
     std::cout << "2. Borrow Book" << std::endl;
     std::cout << "3. Return Book" << std::endl;
     std::cout << "4. View all borrowed books" << std::endl;
-    std::cout << "5. Exit" << std::endl;
+    std::cout << "5. Exit\n" << std::endl;
     std::cout << "Enter the number for the option: " << std::endl;
     std::cin >> option;
     if (option == 1){
@@ -22,6 +43,8 @@ int main(){
       std::cout << "option4" << std::endl;
     } else if (option == 5) {
       exit = 1;
+    } else {
+      std::cout << "Invalid option. Please try again!\n" << std::endl;
     }
   }
   return 0;
